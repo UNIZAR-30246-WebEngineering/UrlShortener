91c6258cac76ebc7abd3d9daf80c066f458d19dd
lAngel <info@langelp.net>
2018-11-30 06:57:40 +0100

    Make IService values extend BaseEntity (for strong typing purposes)

--- demo/src/main/java/urlshortener/demo/service/IService.java
+++ demo/src/main/java/urlshortener/demo/service/IService.java
@@ -1,9 +1,10 @@
 package urlshortener.demo.service;
 
+import urlshortener.demo.domain.BaseEntity;
 import urlshortener.demo.exception.CannotAddEntityException;
 import urlshortener.demo.exception.UnknownEntityException;
 
-public interface IService<K, V> {
+public interface IService<K, V extends BaseEntity<K>> {
 
     void add(V value) throws CannotAddEntityException;
 
