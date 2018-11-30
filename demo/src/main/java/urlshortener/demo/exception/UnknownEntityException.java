442c0a6991d1d3188185edb1b026ef26b04d865c
lAngel <info@langelp.net>
2018-11-30 07:25:27 +0100

    Make exceptions runtime (for code legibility)

--- demo/src/main/java/urlshortener/demo/exception/UnknownEntityException.java
+++ demo/src/main/java/urlshortener/demo/exception/UnknownEntityException.java
@@ -2,7 +2,7 @@ package urlshortener.demo.exception;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2018-11-26T14:20:22.002Z[GMT]")
 
-public class UnknownEntityException extends ApiException {
+public class UnknownEntityException extends RuntimeApiException{
     private int code;
     public UnknownEntityException(int code, String msg) {
         super(code, msg);
