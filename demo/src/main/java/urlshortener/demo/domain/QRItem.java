a557f7ffa80f3233716959405e2c2a936b436751
Víctor Fernández Melic <melic93@gmail.com>
2018-12-05 13:44:45 +0100

    Removed unused imports

--- demo/src/main/java/urlshortener/demo/domain/QRItem.java
+++ demo/src/main/java/urlshortener/demo/domain/QRItem.java
@@ -11,18 +11,12 @@ import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
 // Libraries for QR Generation
-import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.*;
 import com.google.zxing.common.BitMatrix;
 
 import com.google.zxing.client.j2se.*;
 import java.util.Base64;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.awt.image.BufferedImage;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 
 /**
  * QRItem
