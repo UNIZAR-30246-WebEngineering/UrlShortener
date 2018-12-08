f4104565cbf2adc7c9c5a5b940261dbe8f45d215
Víctor Fernández Melic <melic93@gmail.com>
2018-12-08 14:29:42 +0100

    SonarCloud fixes

--- demo/src/main/java/urlshortener/demo/utils/StringChecker.java
+++ demo/src/main/java/urlshortener/demo/utils/StringChecker.java
@@ -2,13 +2,17 @@ package urlshortener.demo.utils;
 
 public class StringChecker {
 
+    private StringChecker() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static int checkString2Int(String string){
         
         if (string == null){
             return 500;
         }
 
-        int num = Integer.valueOf(string);
+        int num = Integer.parseInt(string);
 
         if(num < 30){
             return 500;
