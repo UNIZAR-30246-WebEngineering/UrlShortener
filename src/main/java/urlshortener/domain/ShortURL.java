2c18e1062950fe364fca8542f9d8e7a68d5ab397
fjlopez <fjlopez@unizar.es>
2020-11-09 17:29:09 +0100

    Added Setters

--- src/main/java/urlshortener/domain/ShortURL.java
+++ src/main/java/urlshortener/domain/ShortURL.java
@@ -74,4 +74,43 @@ public class ShortURL {
     return country;
   }
 
+  public void setHash(String hash) {
+    this.hash = hash;
+  }
+
+  public void setTarget(String target) {
+    this.target = target;
+  }
+
+  public void setUri(java.net.URI uri) {
+    this.uri = uri;
+  }
+
+  public void setSponsor(String sponsor) {
+    this.sponsor = sponsor;
+  }
+
+  public void setCreated(java.sql.Date created) {
+    this.created = created;
+  }
+
+  public void setOwner(String owner) {
+    this.owner = owner;
+  }
+
+  public void setMode(Integer mode) {
+    this.mode = mode;
+  }
+
+  public void setSafe(Boolean safe) {
+    this.safe = safe;
+  }
+
+  public void setIp(String ip) {
+    this.ip = ip;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
 }
